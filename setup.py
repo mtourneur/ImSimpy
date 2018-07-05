4234c43386e3cbe843227df9ae82ad9aba38f17b
CORRE david <david.corre@lam.fr>
2018-07-05 12:46:37 +0200

    Added dependencies in setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
+requirements = ['Click>=6.0', 'numpy', 'scipy', 'matplotlib', 'astropy',
+                'jupyter', 'numexpr', 'scikit-image', 'hjson',
+                'astroquery==0.3.5', 'pyregion', 'cython']
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner']
 
 test_requirements = ['pytest', ]
 
